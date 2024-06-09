16c3439386d7d3201a9c29682d54a3a8882ea24d
H4RRY73 <103443967+H4RRY73@users.noreply.github.com>
2024-06-09 16:16:24 -0500

    feat: adding vehicle documents and vehicle insurance.

---
+++ UniRider.API/Profile/Domain/Model/Aggregates/VehicleInsurance.cs
@@ -0,0 +1,28 @@
+using UniRider.API.Profile.Domain.Model.Commands;
+
+namespace UniRider.API.Profile.Domain.Model.Aggregates;
+
+public partial class VehicleInsurance
+{
+    public int Id { get; set; }
+    public string PolicyNumber { get; private set; }
+    public string Insurer { get; private set; }
+    public string StartDate { get; private set; }
+    public string ExpirationDate { get; private set; }
+
+    protected VehicleInsurance()
+    {
+        this.PolicyNumber = string.Empty;
+        this.Insurer = string.Empty;
+        this.StartDate = string.Empty;
+        this.ExpirationDate = string.Empty;
+    }
+    
+    public VehicleInsurance(CreateVehicleInsuranceCommand command)
+    {
+        this.PolicyNumber = command.PolicyNumber;
+        this.Insurer = command.Insurer;
+        this.StartDate = command.StartDate;
+        this.ExpirationDate = command.ExpirationDate;
+    }
+}
